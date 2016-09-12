9207d540dd01169a61467cf6bda65b7f0bae8b57
syleung <lemonzpam@hotmail.com>
2016-09-12 13:03:58 +1000

    Code cleaned up.

--- server.js
+++ server.js
@@ -10,36 +10,13 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
-// app.get('/', function(req, res) {
-//     // console.log(req);
-//
-//     res.send('Yay for Node Girls!');
-// });
-//
-// app.get('/chocolate', function (req, res) {
-//     res.send('Mm chocolate :O');
-// });
-//
-// app.get('/node', function(req, res) {
-//     res.send('Yo Node School!');
-// });
-//
-// app.get('/girls', function(req, res) {
-//     res.send('Yo School for Girls!');
-// });
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/create-post', function(req, res) {
-    // console.log('/create-post');
-    console.log(req.body);
-
     // Read from posts.json
     fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-        // console.log(file.toString());
         var parsedFile = JSON.parse(file);
-        // console.log(parsedFile);
 
         // Add to parsedFile object
         var postedBody = req.body.blogpost;
@@ -49,7 +26,6 @@ app.post('/create-post', function(req, res) {
         var newPost = {};
         newPost[currentTimeStamp] = postedBody;
         var updatedPosts = extend(parsedFile, newPost);
-        // console.log(Object.keys(extendedPosts));
 
         // Write back to file
         writePosts(updatedPosts);
@@ -68,13 +44,10 @@ app.get('/get-posts', function(req, res) {
 
 app.get('/posts/:postId', function (req, res) {
     var postId = req.params.postId;
-    // res.status(200).send('post id: ' + postId);
     fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-        // console.log(file.toString());
         var parsedFile = JSON.parse(file);
         var postContent = parsedFile[postId];
         if (postContent) {
-            // res.status(200).send(postContent);
             res.render('post', { post: postContent});
         } else {
             res.status(404).send('Sorry, post ' + postId + ' does not exist!');
